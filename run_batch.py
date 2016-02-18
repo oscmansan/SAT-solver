362a4e8318da0d81972914c015aca2cb49674688
Oscar Mañas <oscmansan@gmail.com>
2016-02-18 22:55:28 +0100

    Added script for running in batch

---
+++ run_batch.py
@@ -0,0 +1,16 @@
+import subprocess
+import time
+import os
+
+for file in sorted(os.listdir('random3SAT/')):
+    if file.startswith('vars-150'):
+	f = open('random3SAT/' + file)
+	p = subprocess.Popen(['./oscarSAT'], stdin=f, stdout=subprocess.PIPE)
+	print file
+	print '========================'
+	start = time.time()
+	output = p.communicate()[0]
+	end = time.time()
+	for line in output.splitlines():
+	    print(line)
+	print 'Time: ' + str(end-start) + '\n'
