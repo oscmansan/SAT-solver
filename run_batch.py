09529dda248fe671d37c7a22f4ce6c5b125bfa11
Oscar Mañas <oscmansan@gmail.com>
2016-02-28 22:13:43 +0100

    Improved script for running in batch

--- run_batch.py
+++ run_batch.py
@@ -2,15 +2,29 @@ import subprocess
 import time
 import os
 
+def run(cmd,file):
+    f = open('random3SAT/' + file)
+    p = subprocess.Popen(cmd, stdin=f, stdout=subprocess.PIPE)
+    start = time.time()
+    output = p.communicate()[0]
+    end = time.time()
+    elapsed = end-start;
+    for line in output.splitlines():
+	print(line)
+    return elapsed
+
 for file in sorted(os.listdir('random3SAT/')):
     if file.startswith('vars-250'):
-	f = open('random3SAT/' + file)
-	p = subprocess.Popen(['./oscarSAT'], stdin=f, stdout=subprocess.PIPE)
 	print file
 	print '========================'
-	start = time.time()
-	output = p.communicate()[0]
-	end = time.time()
-	for line in output.splitlines():
-	    print(line)
-	print 'Time: ' + str(end-start) + '\n'
+	
+	print 'Running oscarSAT...'
+	oscarSAT_time = run(['./oscarSAT'],file)
+	print '***'
+	print 'Running picosat...'
+	picosat_time = run(['picosat','-n'],file)
+	print '***'
+	
+	print 'oscarSAT time: ' + str(round(oscarSAT_time,3))
+	print 'picosat time: ' + str(round(picosat_time,3))
+	print 'Comparison: ' + str(round(oscarSAT_time/picosat_time,2)) + '\n'
